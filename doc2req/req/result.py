cfe3caa8f06f35fa0f86812dcfcc8aa4c8729c13
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2023-06-10 15:58:50 -0700

    doc(req/result)

--- doc2req/req/result.py
+++ doc2req/req/result.py
@@ -5,6 +5,10 @@ from doc2req.model.point import Point
 from doc2req.req.hi import HandlerInterface
 @dataclass
 class RequestResult:
+    """
+    container that holds the result (requests.Response), handler (Handler) and point (Point)
+    """
+
     res : requests.Response
     handler : HandlerInterface
     point : Point
