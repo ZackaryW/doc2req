3c352848adeafc1a4dc521ad7549e0fad2f44dd2
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2023-06-10 16:32:56 -0700

    fix: setup js and find package

--- setup.py
+++ setup.py
@@ -6,8 +6,12 @@ setup(
     description="apidoc to dataclasses and other useful utilities",
     author="Zackary W",
     url="https://github.com/ZackaryW/doc2req",
-    packages=find_packages(include=["tool/parser.js"]),
-    install_requires=["pydantic", "requests"],
+    packages=find_packages(),
+    # include js
+    package_data={'': ['*.js']},
+    include_package_data=True,
+    install_requires=["pydantic", "requests", "parse", "orjson"],
     python_requires=">=3.9",
     long_description=open("README.md").read(),
 )
+
