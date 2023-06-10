8c4addbeaf66c5d7ad2e2071977e4b6e389b1576
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2023-06-10 12:50:19 -0700

    doc(etc/uobj)

--- doc2req/etc/unpackableObj.py
+++ doc2req/etc/unpackableObj.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel
 
 class UnpackableObj:
-    def unpack(self):
+    """
+    UnpackableObj is a mixin class that adds unpack method
+    """
+
+    def unpack(self)->dict:
+        """
+        returns a dict representation of the object
+        """
         pass
 
 class UnpackableModel(BaseModel, UnpackableObj):
